174e5242cd85ca9c3cd976aae39ae8454d07d97a
matheusvalexd <35822010+matheusvalexd@users.noreply.github.com>
2024-03-29 04:51:49 -0300

    Update and rename preloaderrive.js to preloadrive.js

--- preloadrive.js
+++ preloadrive.js
@@ -23,26 +23,27 @@ lottieContainerAntes.style.cssText = `
     top: 0;
     left: 0;
     background-color: #09090e;
+     border: 2px solid black; 
 `;
 
 
 const lottieContainer = document.createElement('div');
 lottieContainer.id = 'lottieContainer';
 lottieContainer.style.cssText = `
-    width: 100%;
-    height: 100%;
+    width: 90%;
+    height: 90%;
     display: none;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+    overflow: hidden;
 `;
 
 
 document.body.appendChild(overlay); // Adiciona overlay depois de lottieContainer
 document.body.appendChild(lottieContainerAntes); // Adiciona lottieContainer antes de overlay
 document.body.appendChild(lottieContainer); // Adiciona lottieContainer antes de overlay
-
 
 
 function hideOverlay() {
@@ -54,16 +55,24 @@ function hideOverlay() {
     src: 'https://cdn.jsdelivr.net/gh/matheusvalexd/preloaderJSLottie@4e1255c3edf4f7a7f284a2984460cf962e24d9bd/loading.riv',
     canvas: document.createElement('canvas'),
     autoplay: true,
+ 
     stateMachines: 'idlestate',
-    onLoad: () => {
-        r.resizeDrawingSurfaceToCanvas();
+    
+       onLoad: () => {
+           r.resizeDrawingSurfaceToCanvas();
+            canvas.width = lottieContainer.offsetWidth;
+            canvas.height = lottieContainer.offsetHeight;
+            r.layout = new rive.Layout({
+              fit: rive.Fit.FitWidth, // Change to: rive.Fit.Contain, or Cover
+              alignment: rive.Alignment.Center,
+            });
     },
 });
 const canvas = r.canvas;
-canvas.width = lottieContainer.offsetWidth;
-canvas.height = lottieContainer.offsetHeight;
+
 lottieContainer.appendChild(canvas);
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
    hideOverlay(); // Teste sem o setTimeout
